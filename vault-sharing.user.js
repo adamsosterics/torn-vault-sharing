83db84027974753dc30d51f063cee81be454ca6f
Ádám Sosterics <sosterics.adam@gmail.com>
2024-06-15 18:40:50 +0200

    Make balance inputs work like Torn's money inputs
    So we can use shorthands for values and it's also getting nicely formatted.

--- vault-sharing.user.js
+++ vault-sharing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bhaclash's Vault Sharing script
 // @namespace    bhaclash.vault-sharing
-// @version      9
+// @version      10
 // @description  Helps with tracking balances in a shared vault
 // @author       Bhaclash
 // @match        https://www.torn.com/properties.php
@@ -95,8 +95,8 @@
         }
 
         function saveSettings() {
-            let ownBalanceSetting = Number(document.getElementById("vault-sharing-own-start-balance").value);
-            let spouseBalanceSetting = Number(document.getElementById("vault-sharing-spouse-start-balance").value);
+            let ownBalanceSetting = Number(document.getElementById("vault-sharing-own-start-balance").value.replace(/,/g, ""));
+            let spouseBalanceSetting = Number(document.getElementById("vault-sharing-spouse-start-balance").value.replace(/,/g, ""));
             let startTimeSetting = document.getElementById("vault-sharing-start-time").value;
             localStorage.setItem(localStorageKey, JSON.stringify({ startTime: startTimeSetting, ownStartingBalance: ownBalanceSetting, spouseStartingBalance: spouseBalanceSetting }));
         }
@@ -175,6 +175,8 @@
             document.querySelector(".vault-trans-wrap").insertAdjacentHTML("beforebegin", html);
             document.getElementById("vault-sharing-save-settings").addEventListener("click", handleSave);
             document.getElementById("vault-sharing-update-settings").addEventListener("click", handleUpdate);
+            $('input[id="vault-sharing-own-start-balance"]').tornInputMoney({skipBlurCheck: true});
+            $('input[id="vault-sharing-spouse-start-balance"]').tornInputMoney({skipBlurCheck: true});
         }
 
         // Wiring
